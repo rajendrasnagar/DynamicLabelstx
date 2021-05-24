778f9591018a4d9ffc4e788d481c1c6045e65b15
RSN <rajendra.mitu06@gmail.com>
2021-05-24 17:29:11 +0530

    updated file

--- build/create-scratch-org.sh
+++ build/create-scratch-org.sh
@@ -7,6 +7,6 @@ echo $SFDC_SERVER_KEY | base64 -di > keys/server.key
 echo "Authenticating..."
 sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID --jwtkeyfile keys/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a DevHub
 
-#Create a scratch org
+#Create a scratch org.
 echo "Creating the Scratch Org..."
 sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
